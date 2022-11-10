2ba856f856db2dfa6c0b89136e5aff6864a1cbb1
faazizpro <fahadafs11@gmail.com>
2022-11-10 21:54:16 +0600

    Dynamic review created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -20,6 +20,42 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
     try{
+        const foodCollection = client.db('FoodHunter').collection('FoodLists');
+        const reviewCollection = client.db('FoodHunter').collection('FoodReviews');
+
+        app.get('/foods', async(req, res)=>{
+            const query = {};
+            const cursor = foodCollection.find(query);
+            const foods = await cursor.toArray();
+            res.send(foods);
+        })
+        app.get('/allreview', async(req, res)=>{
+            const query = {};
+            console.log(query);
+            const cursor = reviewCollection.find(query);
+            const allreview = await cursor.toArray();
+            res.send(allreview);
+        })
+
+        app.get('/foods/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = { _id : ObjectId(id)};
+            const food = await foodCollection.findOne(query);
+            res.send(food);
+        })
+
+        app.get('/reviewsbyid', async(req, res)=>{
+            const serviceId = req.query.serviceId;
+            const query = { serviceId:serviceId};
+            const reviews = await reviewCollection.find(query).toArray();
+            res.send(reviews);
+        })
+
+        app.post('/reviews', async(req, res)=>{
+            const reviewDetails = req.body;
+            const result = await reviewCollection.insertOne(reviewDetails);
+            res.send(result);
+        })
         
     }
     finally{
